bf7c2ade467eaf744b74ab2c534473a3c2e8a5e7
Trevor Roberts Jr <vmtrooper@gmail.com>
2015-12-14 22:32:57 -0800

    Add destiny app and create class for actions in the functional test

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,34 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Chrome()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+	def setUp(self):
+		self.browser = webdriver.Chrome()
+		self.browser.implicitly_wait(5)
 
-assert 'Django' in browser.title
+	def tearDown(self):
+		# Satisfied, Mike closes the web browser
+		self.browser.quit()
+
+	def test_can_start_a_squad_and_retrieve_it_later(self):
+		# Mike has heard of a site for keeping track of his
+		# Destiny squad mates, and he checks it out.
+		self.browser.get('http://localhost:8000')
+		
+		# He notices the page title and header include the text
+		# "Destiny SquadTracker"
+		self.assertIn('Destiny SquadTracker',self.browser.title)
+		self.fail('Finish the test!')
+		
+		# Mike enters the gamertag of one of his teammates
+		
+		# The gamertag is added to his list of teammates,
+		# and it is a clickable link that will display his
+		# squad mate's light level, weapons, and armor
+		
+		# There is still a textbox to enter more squadmates.
+		# So, he adds another.
+		
+		# The page updates and both squadmates are displayed.
+if __name__ == '__main__':
+	unittest.main(warnings='ignore')
