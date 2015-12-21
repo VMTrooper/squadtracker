995c5900c961826058e6edcb829db5af1ff22447
Trevor Roberts Jr <vmtrooper@gmail.com>
2015-12-21 16:16:43 -0400

    Functional test now checks we can input a squad team member

--- functional_tests.py
+++ functional_tests.py
@@ -18,16 +18,30 @@ class NewVisitorTest(unittest.TestCase):
 		# He notices the page title and header include the text
 		# "Destiny SquadTracker"
 		self.assertIn('Destiny SquadTracker',self.browser.title)
-		self.fail('Finish the test!')
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('Destiny SquadTracker',header_text)
 		
 		# Mike enters the gamertag of one of his teammates
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+			inputbox.get_attribute('placeholder'),
+			'Enter a squad member name'
+			)
+		inputbox.send_keys('RaZaK')
+		inputbox.send_keys(Keys.ENTER)
 		
 		# The gamertag is added to his list of teammates,
 		# and it is a clickable link that will display his
 		# squad mate's light level, weapons, and armor
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue(
+			any(row.text == 'RaZaK' for row in rows)
+			)
 		
 		# There is still a textbox to enter more squadmates.
 		# So, he adds another.
+		self.fail('Finish the test!')
 		
 		# The page updates and both squadmates are displayed.
 if __name__ == '__main__':
