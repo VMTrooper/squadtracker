23ebd51e56f812be2b3d82312ea1936f75975ead
Trevor Roberts Jr <vmtrooper@gmail.com>
2015-12-24 23:15:03 -0400

    first FTs for layout and styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -16,6 +16,28 @@ class NewVisitorTest(LiveServerTestCase):
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
 		self.assertIn(row_text,[row.text for row in rows])
+	def test_layout_and_styling(self):
+		# Mike goes to the home page
+		self.browser.get(self.live_server_url)
+		self.browser.set_window_size(1024,768)
+
+		# He notices the input box is nicely centered
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(
+			inputbox.location['x'] + inputbox.size['width'] / 2,
+			512,
+			delta=5
+		)
+
+		# He starts a new list and sees the input is nicely
+		# centered there too
+		inputbox.send_keys('testing\n')
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(
+			inputbox.location['x'] + inputbox.size['width'] / 2,
+			512,
+			delta=5
+		)
 
 	def test_can_start_a_squad_and_retrieve_it_later(self):
 		# Mike has heard of a site for keeping track of his
